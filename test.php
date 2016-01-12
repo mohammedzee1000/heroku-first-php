91a0e2790251983c68b9a2682ef418913d913485
Mohammed Zeeshan Ahmed <mohammed.zee1000@gmail.com>
2016-01-12 17:58:26 +0530

    Update test.php

--- test.php
+++ test.php
@@ -3,7 +3,7 @@ require_once("sql.php");
 class SQLTest extends PHPUnit_Framework_TestCase {
   public function test() {
     $sql = new SQL();
-    echo "Hello"
+    echo "Hello";
     $doc = $sql->main();
     //$doc = false;
     $this->assertNotEquals($doc, false);
